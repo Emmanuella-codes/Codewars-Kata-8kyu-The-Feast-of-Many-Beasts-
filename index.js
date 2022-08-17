c850a66340782b213c958ae78eeb064199894730
Emmanuella-codes <emmanuella.chukwu77@gmail.com>
2022-08-17 22:05:08 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function feast(beast, dish) {
   let lastChar = beast[beast.length - 1];
   return Boolean(dish[0] === firstChar && dish[dish.length - 1] === lastChar);
 }
-console.log(feast("great blue heron", "garlic naan"));
-console.log(feast("brown bear", "bear claw"));
-console.log(feast("chickadee", "chocolate cake"))
+console.log(feast("great blue heron", "garlic naan")); // returns true
+console.log(feast("brown bear", "bear claw"));      // returns false
+console.log(feast("chickadee", "chocolate cake"))   // returns true
